a42981f9fdd7975459546416860e6d0163a2e083
Eric Couch <james.couch@ruminatetech.com>
2020-06-08 11:13:20 -0500

    added instuctions for assignment

--- volcanos.js
+++ volcanos.js
@@ -3,3 +3,11 @@ const volcanoes = require('./volcanos.json');
 volcanoes.forEach(function(volcano) {
     console.log(volcano.Name);
 })
+
+// Find all volcanos that errupted in the 1970s
+// Find all volcanos that errupted in United States
+// Find volcano that produced the most number of deaths
+// Find number of volcanos by each type of volcano, 
+// e.g. Shield volcano, Stratovolcano, Caldera, etc.
+// Find number of volcanoes by country e.g. United States, 
+// Russia, etc.
